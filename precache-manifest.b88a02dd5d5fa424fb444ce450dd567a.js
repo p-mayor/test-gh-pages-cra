13707b68a7b3e32392c4b82c56cd335b42680bba
Peter Mayor <petermayor@Peters-MacBook-Pro.local>
2020-07-31 23:30:44 -0400

    Updates

---
+++ precache-manifest.b88a02dd5d5fa424fb444ce450dd567a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f854eb1fd8bc97c6762da336215485b9",
+    "url": "/test-gh-pages-cra/index.html"
+  },
+  {
+    "revision": "da60e7bdc47369187c87",
+    "url": "/test-gh-pages-cra/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "44048ae0dd0942cfd878",
+    "url": "/test-gh-pages-cra/static/js/2.9e41738e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-gh-pages-cra/static/js/2.9e41738e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da60e7bdc47369187c87",
+    "url": "/test-gh-pages-cra/static/js/main.15b3b505.chunk.js"
+  },
+  {
+    "revision": "16b3c3400eab5b9d9ff6",
+    "url": "/test-gh-pages-cra/static/js/runtime-main.99334848.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-gh-pages-cra/static/media/logo.5d5d9eef.svg"
+  }
+]);
